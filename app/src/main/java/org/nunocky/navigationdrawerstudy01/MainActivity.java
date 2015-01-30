7b413ffe7c3fc5cb755b971ccc6ba46ce1b7ac82
NUNOKAWA Masato <masato.nunokawa@gmail.com>
2015-01-30 14:02:37 +0900

    Actionbarを取り除く (Toolbarのみに)

--- app/src/main/java/org/nunocky/navigationdrawerstudy01/MainActivity.java
+++ app/src/main/java/org/nunocky/navigationdrawerstudy01/MainActivity.java
@@ -2,34 +2,38 @@ package org.nunocky.navigationdrawerstudy01;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.view.LayoutInflater;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends FragmentActivity {
     private static final String TAG = "MainActivity";
 
     private ListView mDrawerList;
     private String[] mDrawerString;
-    // private Toolbar mToolbar;
+    private Toolbar mToolbar;
     private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.tool_bar);
+        // setSupportActionBar(mToolbar);
+        mToolbar.setTitle(getTitle());
 
         if (savedInstanceState == null) {
             FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
@@ -39,13 +43,14 @@ public class MainActivity extends ActionBarActivity {
 
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
-        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open, R.string.drawer_close);
         mDrawerToggle.setDrawerIndicatorEnabled(true);
-        drawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        // getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        // getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         setupDrawerList();
     }
@@ -53,12 +58,13 @@ public class MainActivity extends ActionBarActivity {
     private void setupDrawerList() {
         mDrawerString = getResources().getStringArray(R.array.solarsystem);
 
-        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_item, mDrawerString));
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mDrawerString));
 
         mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(MainActivity.this, mDrawerString[position], Toast.LENGTH_SHORT).show();
+                mDrawerLayout.closeDrawer(Gravity.LEFT);
             }
         });
     }
@@ -85,19 +91,4 @@ public class MainActivity extends ActionBarActivity {
         mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
 }
